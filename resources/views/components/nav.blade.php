aaf68890a8ed4c6e77020c3cf513341b1489a895
Klimov Code <klimovcode@mail.ru>
2021-06-12 13:18:23 +0500

    finish navigation

--- resources/views/components/nav.blade.php
+++ resources/views/components/nav.blade.php
@@ -4,12 +4,27 @@
         <div class="nav_logo p-2">
             <h1><a href="/" class="font-bold text-2xl">Libteka</a></h1>
         </div>
-        <div id="nav_wrap" class="nav_wrap flex p-2 max-w-xs m-auto justify-between">
-            <div class="nav_parts">
-                <div class="nav_part_select cursor-pointer">Разделы</div>
+        <div id="nav_wrap" class="nav_wrap flex p-2 max-w-xs m-auto">
+            <div id="nav_part" class="nav_parts" v-on:click="togglePartMenu">
+                <div class="nav_part_select relative cursor-pointer flex hover:text-purple-700">
+                    <span class="pl-2">Разделы</span>
+                    <i v-if="!isPartSelect" class="material-icons">arrow_drop_down</i>
+                    <i v-if="isPartSelect" class="material-icons">arrow_drop_up</i>
+                    <div v-if="isPartSelect" class="nav_part_menu absolute bg-gray-100 shadow-lg w-full top-8 p-2 flex flex-col">
+                        <a href="/part/science" class="text-black hover:text-purple-700">Наука</a>
+                    </div>
+                </div>
             </div>
-            <div class="nav_categories">
-                <div class="nav_categories_select cursor-pointer pl-2">Категории</div>
+            <div class="nav_categories" v-on:click="toggleCategoryMenu">
+                <div class="nav_categories_select cursor-pointer pl-4 flex relative hover:text-purple-700">
+                    <span class="pl-2">Категории</span>
+                    <i v-if="!isCategorySelect" class="material-icons">arrow_drop_down</i>
+                    <i v-if="isCategorySelect" class="material-icons">arrow_drop_up</i>
+                    <div v-if="isCategorySelect" class="nav_part_menu absolute bg-gray-100 shadow-lg w-full top-8 p-2 flex flex-col">
+                        <span href="/part/science" class="text-gray-400">Наука</span>
+                        <a href="/category/logic" class="text-black hover:text-purple-700">Логика</a>
+                    </div>
+                </div>
             </div>
             {{-- search --}}
             {{-- forum --}}
@@ -20,7 +35,20 @@
         var app = new Vue({
             el: '#nav_wrap',
             data: {
-                message: 'Libteka'
+                isPartSelect: false,
+                isCategorySelect: false,
+            },
+            methods: {
+                togglePartMenu: function() {
+                    this.isPartSelect ? this.closeAllMenu() : (this.closeAllMenu(), this.isPartSelect = true)
+                },
+                toggleCategoryMenu: function() {
+                    this.isCategorySelect ? this.closeAllMenu() : (this.closeAllMenu(), this.isCategorySelect = true)
+                },
+                closeAllMenu: function() {
+                    this.isPartSelect = false
+                    this.isCategorySelect = false
+                }
             }
         })
     </script>
--- resources/views/layouts/head.blade.php
+++ resources/views/layouts/head.blade.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Libteka.ru | @yield('title', 'Твой путь в развитии')</title>
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
     <script src="https://cdn.jsdelivr.net/npm/vue@2/dist/vue.js"></script>
     <link href="/css/app.css" rel="stylesheet">
 </head>
