0dee80450dc007f936fb9d7b23b98029b47f6d91
Klimov Code <klimovcode@mail.ru>
2021-06-20 14:25:32 +0500

    setting migration for category

--- database/migrations/2021_06_20_091406_create_categories_table.php
+++ database/migrations/2021_06_20_091406_create_categories_table.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
 
 class CreateCategoriesTable extends Migration
 {
@@ -15,7 +16,14 @@ class CreateCategoriesTable extends Migration
     {
         Schema::create('categories', function (Blueprint $table) {
             $table->id();
-            $table->timestamps();
+            $table->text('img');
+            $table->string('imgalt');
+            $table->string('title');
+            $table->text('subtitle');
+            $table->text('description');
+            $table->string('slug');
+            $table->timestamp('created_at')->useCurrent();
+            $table->timestamp('updated_at')->default(DB::raw('CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP'));
         });
     }
 
