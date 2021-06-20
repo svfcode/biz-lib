4a2ff9c7e59a59a6f11fd93eef33ea248fbc63bc
Klimov Code <klimovcode@mail.ru>
2021-06-20 13:41:17 +0500

    create migration for myauth

--- app/Http/Middleware/Myauth.php
+++ app/Http/Middleware/Myauth.php
@@ -4,6 +4,7 @@ namespace App\Http\Middleware;
 
 use Closure;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class Myauth
 {
@@ -16,8 +17,8 @@ class Myauth
      */
     public function handle(Request $request, Closure $next)
     {
-        // $currentSession = session();
-        // dd($currentSession);
+        
+
         return $next($request);
     }
 }
