68082346e44695234af12d8e95862b805294373a
Klimov Code <klimovcode@mail.ru>
2021-06-20 14:53:09 +0500

    add category/create layout

--- app/Http/Controllers/AdminCategoriesController.php
+++ app/Http/Controllers/AdminCategoriesController.php
@@ -24,7 +24,7 @@ class AdminCategoriesController extends Controller
      */
     public function create()
     {
-        //
+        return view('admin.category.create');
     }
 
     /**
@@ -35,7 +35,7 @@ class AdminCategoriesController extends Controller
      */
     public function store(Request $request)
     {
-        //
+
     }
 
     /**
