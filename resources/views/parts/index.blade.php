53b50d825ba7e978b44970bb363fdf6bed0b3e2b
Klimov Code <klimovcode@mail.ru>
2021-06-27 22:02:42 +0500

    work on deploy

--- resources/views/parts/index.blade.php
+++ resources/views/parts/index.blade.php
@@ -4,7 +4,7 @@
 
 @section('content')
     <div class="md:container md:mx-auto px-4 mt-8">
-        <h2 class="text-2xl text-center">Разделы</h2>
+        <h1 class="text-2xl text-center">Разделы </h1>
         @include('parts.cards', [$parts])
     </div>
 @endsection
