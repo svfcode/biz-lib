7597ae40eba9f29171dd982bcac76a0c6cc55499
Klimov Code <klimovcode@mail.ru>
2021-07-02 06:37:40 +0500

    worked parse through tor

--- tools/parse.py
+++ tools/parse.py
@@ -1,9 +1,22 @@
 import requests
 from bs4 import BeautifulSoup
 
-url = 
-proxies = {
-    'http':  'socks5://127.0.0.1:9052',
-    'https': 'socks5://127.0.0.1:9052'
-}
-print(session.get("http://httpbin.org/ip").text)
+def get_tor_session():
+    session = requests.session()
+    # Tor uses the 9050 port as the default socks port
+    session.proxies = {
+        'http':  'socks5h://127.0.0.1:9050',
+        'https': 'socks5h://127.0.0.1:9050'
+    }
+    return session
+
+session = get_tor_session()
+
+url = 'http://flibustahezeous3.onion/b/136049'
+print("Getting ...", url)
+response = session.get(url)
+soup = BeautifulSoup(response.content, 'html.parser')
+
+title = soup.find("h1", class_="title").text
+
+print(title)
