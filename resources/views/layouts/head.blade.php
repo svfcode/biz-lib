227684672ee17a56c2b366d2deb4d4aeb3bf07ff
Klimov Code <klimovcode@mail.ru>
2021-07-09 21:12:29 +0500

    update favicon

--- resources/views/layouts/head.blade.php
+++ resources/views/layouts/head.blade.php
@@ -8,7 +8,16 @@
     <meta name="description" content="@yield('description', 'Сайт с подборками книг для развития по темам')">
     @yield('noindex')
     <title>Libteka.ru | @yield('title', 'Твой путь в развитии')</title>
+
     <link rel="icon" href="https://libteka.ru/favicon.ico" type="image/x-icon">
+    <link rel="apple-touch-icon" sizes="180x180" href="https://libteka.ru/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="https://libteka.ru/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="https://libteka.ru/favicon-16x16.png">
+    <link rel="manifest" href="https://libteka.ru/site.webmanifest">
+    <link rel="mask-icon" href="https://libteka.ru/safari-pinned-tab.svg" color="#5bbad5">
+
+    <meta name="msapplication-TileColor" content="#da532c">
+    <meta name="theme-color" content="#ffffff">
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
     <script src="https://cdn.jsdelivr.net/npm/vue@2/dist/vue.js"></script>
     <link href="/css/app.css?v=1" rel="stylesheet">
