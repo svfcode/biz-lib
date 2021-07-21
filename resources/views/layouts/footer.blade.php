86b146176cbb3535ac39115eaa9c9602f99471ff
Klimov Code <klimovcode@mail.ru>
2021-07-21 17:51:09 +0500

    done html sitemap

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,8 +1,8 @@
 
-<div class="footer flex bg-gray-100 shadow-lg p-2 flex justify-around">
-    <div class="footer_wrap">
-        <a href="/forauthors" class="text-gray-400">Для правообладателей</a>
-        <a href="/sitemap.html" class="text-gray-400 ml-4">Карта сайта</a>
+<div class="footer flex bg-gray-100 shadow-lg p-2 flex justify-around mt-8">
+    <div class="footer_wrap flex flex-wrap">
+        <a href="/forauthors" class="text-gray-400">Для&nbsp;правообладателей</a>
+        <a href="/sitemap.html" class="text-gray-400 ml-4">Карта&nbsp;сайта</a>
         {{-- <a href="/forbook">Какими программы открывать книги</a> --}}
     </div>
     <a href="http://klimovcode.online" class="text-gray-200 hover:text-gray-700">KlimovCode</a>
--- resources/views/pages/sitemaphtml.blade.php
+++ resources/views/pages/sitemaphtml.blade.php
@@ -10,7 +10,7 @@
 
         <h1 class="text-2xl text-center">Карта сайта</h1>
 
-        <div class="w-full px-2 mx-auto mt-8 md:w-3/5">
+        <div class="w-full px-2 mx-auto mt-8 md:w-10/12">
             <section class="shadow row">
                 <div class="tabs">
 
@@ -18,13 +18,13 @@
 
                     <div class="border-b tab">
                         <div class="border-l-2 border-transparent relative">
-                            <input class="w-full h-full absolute z-10 cursor-pointer opacity-0" type="checkbox" id="chck1">
-                            <header class="flex justify-between items-center p-5 pl-8 pr-8 cursor-pointer select-none tab-label" for="chck1">
+                            <input class="w-full h-20 absolute z-10 cursor-pointer opacity-0" type="checkbox" id="chck1">
+                            <header class="flex justify-between items-center p-2 cursor-pointer select-none tab-label" for="chck1">
                                 <div class="flex flex-wrap items-end">
                                     <h2 class="text-grey-darkest font-thin text-xl">
                                         {{ $part->title }}
                                     </h2>
-                                    <a href="/parts/{{science}}" class="ml-4 text-blue-400 hover:text-blue-900 hover:underline z-20 text-sm">[ Перейти в раздел ]</a>
+                                    <a href="/parts/{{ $part->slug }}" class="ml-4 text-blue-400 hover:text-blue-900 hover:underline z-20 text-sm">[ Перейти в раздел ]</a>
                                 </div>
                                 <div class="rounded-full border border-grey w-7 h-7 flex items-center justify-center test">
                                     <!-- icon by feathericons.com -->
@@ -35,19 +35,19 @@
                                 </div>
                             </header>
                             <div class="tab-content">
-                                <div class="pl-8 pr-8 pb-5 text-grey-darkest">
+                                <div class="p-2 text-grey-darkest">
 
                     @foreach ($cats as $cat)
                     @if ($cat->part_id == $part->id)
                     <div class="border-b tab">
                         <div class="border-l-2 border-transparent relative">
-                            <input class="w-full h-full absolute z-10 cursor-pointer opacity-0" type="checkbox" id="chck1">
-                            <header class="flex justify-between items-center p-5 pl-8 pr-8 cursor-pointer select-none tab-label" for="chck1">
+                            <input class="w-full h-20 absolute z-10 cursor-pointer opacity-0" type="checkbox" id="chck1">
+                            <header class="flex justify-between items-center p-2 cursor-pointer select-none tab-label" for="chck1">
                                 <div class="flex flex-wrap items-end">
                                     <h3 class="text-grey-darkest font-thin text-base">
                                         {{ $cat->title }}
                                     </h3>
-                                    <a href="/" class="ml-4 text-blue-400 hover:text-blue-900 hover:underline z-20 text-sm">[ Перейти в категорию ]</a>
+                                    <a href="/parts/{{ $part->slug }}/categories/{{ $cat->slug }}" class="ml-4 text-blue-400 hover:text-blue-900 hover:underline z-20 text-sm">[ Перейти в категорию ]</a>
                                 </div>
                                 <div class="rounded-full border border-grey w-7 h-7 flex items-center justify-center test">
                                     <!-- icon by feathericons.com -->
@@ -58,9 +58,18 @@
                                 </div>
                             </header>
                             <div class="tab-content">
-                                <div class="pl-8 pr-8 pb-5 text-grey-darkest">
+                                <div class="p-2 text-grey-darkest">
 
-
+                    @foreach ($books as $book)
+                    @if ($book->cat_id == $cat->id)
+                    <div class="flex flex-col">
+                        <a href="/parts/{{ $part->slug }}/categories/{{ $cat->slug }}/{{ $book->slug }}"
+                            class="ml-4 text-blue-400 hover:text-blue-900 hover:underline z-20 text-sm">
+                            {{ $book->title }}
+                        </a>
+                    </div>
+                    @endif
+                    @endforeach
 
                                 </div>
                             </div>
